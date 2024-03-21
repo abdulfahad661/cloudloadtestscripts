09eef5ef8373f053d9864982d8f4c2198470927a
Abdul Fahad <136641221+abdulfahad661@users.noreply.github.com>
2024-03-21 16:23:31 +0500

    Update cpu_spike.py

--- cpu_spike.py
+++ cpu_spike.py
@@ -24,3 +24,5 @@ def simulate_cpu_spike(duration=30, cpu_percent=80):
 if __name__ == '__main__':
     # Simulate a CPU spike for 30 seconds with 80% CPU utilization
     simulate_cpu_spike(duration=30, cpu_percent=80)
+#Uage guide
+#https://www.youtube.com/watch?v=u4XngwbY-O0
